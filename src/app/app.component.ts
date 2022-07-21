9957da38c6cf5f65c84d750ea5f85ffb9b6d8ec2
jyotirmoy.p@eccentricengine.com <jyotirmoypal077@gmail.com>
2022-07-21 23:26:02 +0530

    master : initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
-
+import { MatCalendarCellCssClasses } from '@angular/material/datepicker';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angMat';
+  selectedDate: any;
+  minDate = new Date()
+  today = new Date()
+  datesToHighlight = [
+    '2019-01-22T18:30:00.000Z',
+    '2019-01-22T18:30:00.000Z',
+    '2019-01-24T18:30:00.000Z',
+    '2019-01-28T18:30:00.000Z',
+    '2019-01-24T18:30:00.000Z',
+    '2019-01-23T18:30:00.000Z',
+    '2019-01-22T18:30:00.000Z',
+    '2019-01-25T18:30:00.000Z',
+  ];
+
+  constructor(public datepipe: DatePipe){
+    let date :any= this.datepipe.transform(this.today, 'yyyy-MM-dd');
+    console.log(date);
+    this.minDate = new Date(date)
+    console.log( this.minDate);
+    this.selectedDate = this.minDate
+  }
+
+  ngOnInit() {
+    this.datesToHighlight = []
+    console.log();
+    
+    // this.datesToHighlight.push(this.minDate.toString())
+  }
+
+  onSelect(event:any) {
+    console.log(event);
+    this.selectedDate = event;
+  }
+
+  // dateClass() {
+  //   console.log('HERE');
+  //   return (date: Date): MatCalendarCellCssClasses => {
+  //     const highlightDate = this.datesToHighlight
+  //       .map((strDate) => new Date(strDate))
+  //       .some(
+  //         (d) =>
+  //           d.getDate() === date.getDate() &&
+  //           d.getMonth() === date.getMonth() &&
+  //           d.getFullYear() === date.getFullYear()
+  //       );
+
+  //     return highlightDate ? 'special-date' : '';
+  //   };
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import { DatePipe } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
